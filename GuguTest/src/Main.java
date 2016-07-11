0e0fa972389d977c2499585abf2b7364fea9c2c4
juno <juno@juno-PC>
2016-07-11 16:52:17 +0900

    Test

---
+++ GuguTest/src/Main.java
@@ -0,0 +1,15 @@
+
+
+public class Main {
+	public static void main(String[] args){
+		for(int iCount = 2; iCount < 10; iCount++){
+				System.out.println(iCount+"단-------------------------------------------");
+			for(int jCount =1; jCount <10; jCount++){
+				System.out.println(iCount+"X"+jCount+"="+iCount*jCount);
+				
+			}
+		}
+		System.out.println("githubtest");
+	}
+	
+}
